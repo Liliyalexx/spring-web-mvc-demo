db92331d4dbf92a13eee448e47db09ae705c9544
Liliyalexx <liliya.fedyurina@gmail.com>
2021-04-28 10:04:29 -0700

    Add files via upload

---
+++ spring_web_mvc_demo/src/main/java/com/app/controller/WelcomeController.java
@@ -0,0 +1,31 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class WelcomeController {
+	@RequestMapping("/welcome")
+	public String sayHelloByNameGet(@RequestParam String name, ModelMap map) {
+		map.addAttribute("user", name);
+		map.addAttribute("request", "GET");
+
+		return "dashboard";
+	}
+		
+		@RequestMapping(value="/liliya", method = RequestMethod.POST)
+		
+		public String sayHelloByNamePost(@RequestBody String name, ModelMap map) {
+			map.addAttribute("user", name);
+			map.addAttribute("request", "POST");
+			return "dashboard";
+			
+		
+	}
+
+}
---
+++ spring_web_mvc_demo/src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Welcome</title>
+</head>
+<body>
+<h1> Hello and Welcome to Spring WEB MVC</h1>
+<form action="welcome">
+ Enter Name<input type="text" name="name">
+ <input type="submit" value ="Click">
+</form>
+<br/><br/><br/><br/><br/><br/>
+<form action="liliya" method="post">
+ Enter Name<input type="text" name="name">
+ <input type="submit" value ="Click here for POST">
+</form>
+</body>
+</html>
